1daeeb45799ba943c73a4a7e63f0fa0e35bd33fb
Bryan.Hankins <Bryan.Hankins@navihealth.com>
2019-01-30 13:41:38 -0600

    Completed Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,13 @@ import { ProductService } from './product.service';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 import { ProductListComponent } from './product-list/product-list.component';
 
+import { RouterModule, Routes, Router } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'products', component: ProductListComponent },
+  { path: 'product/:id', component: ProductPageComponent },
+  { path: '', redirectTo: 'products', pathMatch: 'full' }
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -21,7 +28,8 @@ import { ProductListComponent } from './product-list/product-list.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule, 
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
